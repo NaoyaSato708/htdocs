753da44b6371ec7e259447831799b3a9d360514f
NaoyaSato708 <NaoyaSato708@users.noreply.github.com>
2022-06-23 18:12:55 +0900

    ver.2.1

--- result.php
+++ result.php
@@ -3,7 +3,6 @@
 <title>結果画面</title>
 <h1>購入結果</h1>
 <section>
-    <h2>登録完了</h2>
     <button onclick="location.href='purchase.php'">戻る</button>
 </section>
 
@@ -18,14 +17,57 @@ $pdo = new PDO(
 );
 ?>
 
+<h3>購入履歴</h3>
 <?php
+if(!empty($_POST["a"]) && !empty($_POST["b"]) && !empty($_POST["c"])) {
+$pdo = new PDO(
+    "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
+);
+/*
+if ($pdo) {
+    echo "DB接続OK<br>";
+} else {
+    echo "DB接続NG<br>";
+}
+*/
 $pdo -> query("INSERT INTO
 client_info
 (client_name,product_name,price)
 VALUES
 ('$kokyaku','$syohin','$nedan')");
+/*
+if ($pdo) {
+    echo "登録成功<br>";
+} else {
+    echo "登録失敗<br>";
+}
+*/
 ?>
 
+<?php
+$n = $pdo -> query("SELECT * FROM client_info ORDER BY id DESC");
+while ($i = $n -> fetch()) {
+print "顧客名：{$i['client_name']}  商品名：{$i['product_name']}  値段：{$i['price']}円<br><hr>";}
+}
+else 
+{
+    echo 'エラー！';
+    echo '<br/>';
+    $pdo = new PDO(
+        "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
+    );
+    /*
+    if ($pdo) {
+        echo "DB接続OK<br>";
+    } else {
+        echo "DB接続NG<br>";
+    }
+    */
+    $n = $pdo -> query("SELECT * FROM client_info ORDER BY id DESC");
+    while ($i = $n -> fetch()) {
+    print "顧客名：{$i['client_name']}  商品名：{$i['product_name']}  値段：{$i['price']}円<br><hr>";}
+}
+?>
 
 <?php//顧客毎の購入履歴を求める?>
 <h3>顧客別購入商品一覧</h3>
@@ -72,50 +114,4 @@ $g = $pdo -> query("SELECT SUM(price) as sum_price FROM client_info WHERE client
 while ($h = $g -> fetch()) {
 print "合計金額：{$h['sum_price']}円<br><hr><br>";}
 */
-?>
-
-<h3>購入履歴</h3>
-<?php
-if(!empty($_POST["a"]) && !empty($_POST["b"]) && !empty($_POST["c"])) {
-$pdo = new PDO(
-    "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
-);
-/*
-if ($pdo) {
-    echo "DB接続OK<br>";
-} else {
-    echo "DB接続NG<br>";
-}
-*/
-
-/*
-if ($pdo) {
-    echo "登録成功<br>";
-} else {
-    echo "登録失敗<br>";
-}
-*/
-?>
-
-<?php
-$n = $pdo -> query("SELECT * FROM client_info ORDER BY id DESC");
-while ($i = $n -> fetch()) {
-print "顧客名：{$i['client_name']}  商品名：{$i['product_name']}  値段：{$i['price']}円<br><hr>";}
-}
-else 
-{
-    $pdo = new PDO(
-        "mysql:dbname=hello_world;host=localhost","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
-    );
-    /*
-    if ($pdo) {
-        echo "DB接続OK<br>";
-    } else {
-        echo "DB接続NG<br>";
-    }
-    */
-    $n = $pdo -> query("SELECT * FROM client_info ORDER BY id DESC");
-    while ($i = $n -> fetch()) {
-    print "顧客名：{$i['client_name']}  商品名：{$i['product_name']}  値段：{$i['price']}円<br><hr>";}
-}
 ?>
